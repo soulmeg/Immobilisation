f8ba24de66a2861387f4427816128717b364d4ea
notyours <117538726+soulmeg@users.noreply.github.com>
2024-01-20 23:06:31 +0300

    config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Service/ServiceModele');
+$autoload['model'] = array();
--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/PROJETS/commercial_s5_v2/';
-$config['base_url'] = 'http://localhost/PROJETS/stock_entreprise/';
+$config['base_url'] = 'http://localhost/PROJETS/Immobilisation/';
 
 /*
 |--------------------------------------------------------------------------
